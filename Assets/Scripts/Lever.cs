78f99175da04a98dbb5435f6aeab8e8d3fd29852
Michmelangio <me@bystrikov.ru>
2026-06-26 02:00:14 +0300

    Анимация падения, началдьные заставки, иконка, механика тумблера, разные мелкие правки и оптимизация кода.

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,55 @@
+using DG.Tweening;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] float _angleOn;
+    [SerializeField] float _angleOff;
+    [SerializeField] Transform _lewer;
+    [SerializeField] PlayerInput _input;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _audioClip;
+    [SerializeField] Renderer _lightRenderer;
+    private bool _isPlayerInside = false;
+    private bool _isTurn = false;
+    private InputAction _inputAction;
+    private void Start()
+    {
+        _inputAction = _input.actions.FindAction("Use");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isPlayerInside = true;
+            Debug.Log("Игрок вошел в зону!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isPlayerInside = false;
+            Debug.Log("Игрок вышел из зоны!");
+        }
+    }
+    private void RotateLever()
+    {
+        _isTurn = !_isTurn;
+        _lewer.DOKill();
+        _lewer.DOLocalRotate(new Vector3(_isTurn ? _angleOn : _angleOff, 90.0f, 0.0f), 0.2f);
+        _lightRenderer.materials[0].DOColor(_isTurn ? new Color(0.0f, 255.0f, 0.0f, 10.0f) : new Color(255.0f, 0.0f, 0.0f, 1.0f), 0.2f);
+        _audioSource.PlayOneShot(_audioClip);
+    }
+    private void Update()
+    {
+        if (_isPlayerInside && _inputAction.WasPressedThisFrame())
+        {
+            Debug.Log("123");
+            RotateLever();
+        }
+    }
+}
